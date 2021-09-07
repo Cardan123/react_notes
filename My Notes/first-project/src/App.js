34d7b916dbc35513e2f2f008c0914211c94bc3ec
Cardan <cosasdecardan@gmail.com>
2021-09-07 17:37:12 -0500

    first project initial

---
+++ My Notes/first-project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
