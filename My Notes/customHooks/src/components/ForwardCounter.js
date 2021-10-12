63f4829e38ec5d0cc90e7a981341d90c7ef5e943
Cardan <cosasdecardan@gmail.com>
2021-10-12 16:23:05 -0500

    adding customHooks

---
+++ My Notes/customHooks/src/components/ForwardCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Card from "./Card";
+import useCounter from "../hooks/use-counter";
+
+const ForwardCounter = () => {
+  const counter = useCounter(true);
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
