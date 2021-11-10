70844b63644345b5029ce1f9df0500e83b7504b7
Cardan <cosasdecardan@gmail.com>
2021-11-09 18:11:53 -0600

    adding connection with firebase and form handling to mealProject

---
+++ My Notes/mealProject2/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-69383-default-rtdb.firebaseio.com/meal.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+      const loadMeals = [];
+
+      for (const key in responseData) {
+        loadMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.mealsError}>{httpError}</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
